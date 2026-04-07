a1849cfddf771881384516ab62da1d3ee9fe0b36
Taneesha <taneesha.mehra@icloud.com>
2026-04-07 19:36:15 +0100

    Added a new route

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ app.get("/",(req,res,next)=>{
     res.status(200).send("This is the home page")
 })
 
+app.post("/add-expense",(req,res,next)=>{
+    console.log(req.Body)
+    res.status(200).send("Expense added successfully")
+})
+
 app.listen(PORT ,()=>{
     console.log("Listening on PORT: " + PORT)
 })
+
